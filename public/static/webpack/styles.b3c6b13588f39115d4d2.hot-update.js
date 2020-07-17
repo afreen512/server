8460b340fe3a25ec651706c1e994b9f63f5ebd97
afreen512 <afreenhussain866@gmail.com>
2020-07-17 23:49:13 +0500

    server

---
+++ public/static/webpack/styles.b3c6b13588f39115d4d2.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate("styles",{
+
+/***/ "./node_modules/react-quill/dist/quill.bubble.css":
+false,
+
+/***/ 11:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9zdHlsZXMuYjNjNmIxMzU4OGYzOTExNWQ0ZDIuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
